ec6dc10911c72bd97778e25485ddd0c866ce548a
shivi150ka <pes2003annu@gmail.com>
2025-03-18 23:35:43 +0530

    java

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+let pencilprrice=5;
+let eraser=10;
+console.log("price=" ,pencilprrice+eraser);
+console.log(`total price ${pencilprrice+eraser}`);//template literal 
+console.log(pencilprrice**eraser);
+let n=5;
+let str="5";//compare value not type
+console.log(n===str);//compare value and type
+let stri='a=61&aA=41'//compare unique code
+let colour="red";
+if(colour==red){
+    console.log("stop");
+}
+if(colour==yellow){
+    console.log("stop");}
+    let color="red";
+    switch(color){
+        case "red":{
+            console.log("stop");
+        }
+    }
+    alert("something is wrp");
+    console.error("error");
+    console.warn("nothing");
+   let firstname= prompt("enter your name");
+   console.log(firstname);
