f92a34a759d761931a02baf50f78d3dc3e2ceeef
shivi150ka <pes2003annu@gmail.com>
2025-04-14 16:30:45 +0530

    stringfnjs

---
+++ practice.js
@@ -0,0 +1,25 @@
+let number=50;
+if(number%10==0){
+    console.log("good");
+}
+else{
+    console.log("bad");
+}
+
+let username=prompt("enter your username");
+let age=prompt("enter your age");
+alert(`${username} is ${age} years old`);
+let goldenstring=auyt ;
+if((goldenstring[0]=='A'||'a') && (goldenstring.length>5)){
+
+}
+let md= "   hel lo  ";/*new string is formed immutable*/ 
+console(md.trim());
+console(md.toLowerCase());/*sara small me convert hoga  */
+console(md.toUpperCase());/*saara big me ho jayega */
+console(md.indexOf(he));
+let newmsg=md.trim().toUpperCase();
+console.log(newmsg);/*left to right */
+console.log(md.slice(0,3));/* -ve no ko str.length-ve number*/
+console.log(md.replace("he","do"));/*first occurence ka replace hoga */
+console.log(md.repeat(3));
