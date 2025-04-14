9da18a4aa9a441b693266657fa58f8ed9b9696b9
shivi150ka <pes2003annu@gmail.com>
2025-04-14 16:55:32 +0530

    arraysmethods

---
+++ arrays.js
@@ -0,0 +1,19 @@
+let studentname=["aman","chho","raju"];
+console.log(studentname[0]);/*type of array is object */
+let info=["annu", 97.6,17];
+let arr=[];
+console.log(studentname[0][0]);/*Arrays are mutuable */
+/*push(add to last), pop, unshift(add to start),shift*/
+console.log(studentname.push(priity));
+console.log(studentname.pop());
+console.log(studentname.shift());
+console.log(studentname.unshift(ary));
+/*indexOf() includes("red")(return true or false value);*/
+let primary=["red","yellow","green"];
+let secondary=["orange","pink",];
+console.log(primary.concat(secondary));
+console.log(primary.reverse());
+console.log(primary.slice(2));
+
+
+
