5539986c765d1c4616f7ad77cbd7af97cdf92e0f
shivi150ka <pes2003annu@gmail.com>
2025-06-17 09:20:43 +0530

    web

--- await.js
+++ await.js
@@ -9,6 +9,26 @@ hello()
     console.log("there is an error",err);
 }
 );
-let demo =()=>{
+let demo =async()=>{
     return 5;
 };
+
+function getNum(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let number= Math.floor(Math.random()*10)+1;
+            console.log(num);
+            resolve();
+
+        },1000);
+    });
+
+}
+async function demo(params) {
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    getNum();
+}
+demo();
